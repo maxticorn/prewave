b1fde2fa26721402dbdb6fe919a838d5baa2261a
maxticorn <maksticorn@gmail.com>
2024-11-05 12:10:59 +0000

    done

--- src/main/scala/domain/Language.scala
+++ src/main/scala/domain/Language.scala
@@ -2,27 +2,31 @@ package domain
 
 import enumeratum._
 
-/** decided to present this type in the form of enum for type safety 
+/** 
+ * decided to present this type in the form of enum for type safety
  * included all languages that were found during the use of the API
  * if the api starts returning new languages, modifications will be necessary
+ * 
+ * (in reality I would separate this part into API library
+ * to assign the responsibility for revision to the API developers)
  */
-sealed trait Language extends EnumEntry
+sealed abstract class Language(override val entryName: String) extends EnumEntry
 
 case object Language extends Enum[Language] with CirceEnum[Language]:
-  case object en extends Language
+  case object English extends Language("en")
 
-  case object de extends Language
+  case object German extends Language("de")
 
-  case object es extends Language
+  case object Spanish extends Language("es")
 
-  case object it extends Language
+  case object Italian extends Language("it")
 
-  case object pt extends Language
+  case object Portuguese extends Language("pt")
 
-  case object ar extends Language
+  case object Arabic extends Language("ar")
 
-  case object no extends Language
+  case object Vietnamese extends Language("ên")
 
-  case object `ên` extends Language
+  case object Norwegian extends Language("no")
 
   val values = findValues
