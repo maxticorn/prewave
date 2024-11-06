59a7dac47ba527ed614d63f42e7dfa11aa9c11f7
maxticorn <maksticorn@gmail.com>
2024-11-06 01:40:16 +0000

    simple unit tests

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "3.3.4"
 
 val ceVersion = "3.5.5"
+val ceTestVersion = "1.5.0"
 val enumeratumVersion = "1.7.5"
 val pureConfigVersion = "0.17.7"
 val sttpVersion = "3.10.1"
@@ -11,6 +12,10 @@ val circeVersion = "0.14.1"
 val catsEffect = List(
   "org.typelevel" %% "cats-effect"
 ).map(_ % ceVersion)
+
+val ceTest = List(
+  "org.typelevel" %% "cats-effect-testing-scalatest"
+).map(_ % ceTestVersion % Test)
 
 val enumeratum = List(
   "com.beachape" %% "enumeratum",
@@ -38,6 +43,7 @@ lazy val root = (project in file("."))
     name := "prewave",
     libraryDependencies ++=
       catsEffect ++
+        ceTest ++
         enumeratum ++
         pureConfig ++
         sttp ++
---
+++ src/test/scala/AlertServiceSpec.scala
@@ -0,0 +1,52 @@
+import cats.effect.*
+import cats.effect.testing.scalatest.AsyncIOSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.freespec.AsyncFreeSpec
+import client.*
+import service.*
+import domain.*
+
+import java.time.LocalDateTime
+
+// for some reason, only class works here; object will not be detected by sbt
+class AlertServiceSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers {
+  "AlertService" - {
+    "return empty if everything is empty" in {
+      val emptyApiClient = new PrewaveApiClient {
+        def getQueryTerms: IO[Set[QueryTerm]] = IO.pure(Set.empty)
+
+        def getAlerts: IO[List[Alert]] = IO.pure(List.empty)
+      }
+      val emptyAlertsService = new AlertsServiceImpl(emptyApiClient)
+      emptyAlertsService.findTerms.asserting(_ shouldEqual Map.empty)
+    }
+
+    "return empty if client throws" in {
+      val emptyApiClient = new PrewaveApiClient {
+        def getQueryTerms: IO[Set[QueryTerm]] = IO.raiseError(new Throwable())
+
+        def getAlerts: IO[List[Alert]] = IO.raiseError(new Throwable())
+      }
+      val emptyAlertsService = new AlertsServiceImpl(emptyApiClient)
+      emptyAlertsService.findTerms.asserting(_ shouldEqual Map.empty)
+    }
+
+    "find terms" in {
+      val emptyApiClient = new PrewaveApiClient {
+        def getQueryTerms: IO[Set[QueryTerm]] =
+          IO.pure(Set(
+            QueryTerm(0, "test term", Language.English, true),
+            QueryTerm(1, "ordered test", Language.English, true),
+            QueryTerm(2, "not found", Language.English, true)
+          ))
+
+        def getAlerts: IO[List[Alert]] =
+          IO.pure(List(
+            Alert("a", List(Content("ordered test term search", ContentType.text, Language.English)), LocalDateTime.now(), InputType.link)
+          ))
+      }
+      val emptyAlertsService = new AlertsServiceImpl(emptyApiClient)
+      emptyAlertsService.findTerms.asserting(_ shouldEqual Map("a" -> Set(0, 1)))
+    }
+  }
+}
